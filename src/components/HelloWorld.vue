0ec8443d7d7c46e14b2267c818ca099a92d3b849
Kikosinaga <ahqusi358@gmail.com>
2025-05-07 17:14:33 +0700

    Menampilkan kegiatan yang sudah di tambahkan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { ref } from 'vue'
 
+const activities = ref(["olahraga","Makan","Tidur"])
+const newActivity = ref('')
 </script>
 
 <template>
-  
+  <div>
+    <input type="text" v-model="newActivity" placeholder="Masukkan kegiatan" />
+    <button>Tambah Kegiatan</button>
+    <ul>
+      <li v-for="(activity, index) in activities" :key="index">{{ activity }}</li>
+    </ul>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
