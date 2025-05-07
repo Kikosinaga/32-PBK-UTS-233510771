20329eaccd3a97f30a6ea2262c78082b429d54d6
Kikosinaga <ahqusi358@gmail.com>
2025-05-07 17:19:26 +0700

    Menambahkan tombol tambah kegiatan untuk menambahkan kegiatan baru ke dalam daftar kegiatan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,12 +3,19 @@ import { ref } from 'vue'
 
 const activities = ref(["olahraga","Makan","Tidur"])
 const newActivity = ref('')
+
+function addActivity() {
+  if (newActivity.value.trim() !== '') {
+    activities.value.push(newActivity.value.trim())
+    newActivity.value = ''
+  }
+}
 </script>
 
 <template>
   <div>
     <input type="text" v-model="newActivity" placeholder="Masukkan kegiatan" />
-    <button>Tambah Kegiatan</button>
+    <button @click="addActivity">Tambah Kegiatan</button>
     <ul>
       <li v-for="(activity, index) in activities" :key="index">{{ activity }}</li>
     </ul>
