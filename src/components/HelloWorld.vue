0653dd98b77617611fa61c78105f240067477252
Kikosinaga <ahqusi358@gmail.com>
2025-05-07 17:36:02 +0700

    Menambahkan fitur filter kegiatan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
+
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const activities = ref([
   { text: "olahraga", done: false },
@@ -7,6 +8,7 @@ const activities = ref([
   { text: "Tidur", done: false }
 ])
 const newActivity = ref('')
+const filter = ref('all') // all, done, notdone
 
 function addActivity() {
   if (newActivity.value.trim() !== '') {
@@ -15,24 +17,40 @@ function addActivity() {
   }
 }
 
-function removeActivity(index) {
-  activities.value.splice(index, 1)
+function removeActivity(activity) {
+  const index = activities.value.indexOf(activity)
+  if (index > -1) {
+    activities.value.splice(index, 1)
+  }
 }
 
-function toggleDone(activity) {
-  activity.done = !activity.done
-}
+// Removed toggleDone function as it is unused
+
+const filteredActivities = computed(() => {
+  if (filter.value === 'done') {
+    return activities.value.filter(a => a.done)
+  } else if (filter.value === 'notdone') {
+    return activities.value.filter(a => !a.done)
+  } else {
+    return activities.value
+  }
+})
 </script>
 
 <template>
   <div>
     <input type="text" v-model="newActivity" placeholder="Masukkan kegiatan" />
     <button @click="addActivity">Tambah Kegiatan</button>
+    <div>
+      <button @click="filter = 'all'">Semua</button>
+      <button @click="filter = 'done'">Selesai</button>
+      <button @click="filter = 'notdone'">Belum Selesai</button>
+    </div>
     <ul>
-      <li v-for="(activity, index) in activities" :key="index">
+      <li v-for="(activity, index) in filteredActivities" :key="index">
         <input type="checkbox" v-model="activity.done" />
         <span :style="{ textDecoration: activity.done ? 'line-through' : 'none' }">{{ activity.text }}</span>
-        <button @click="removeActivity(index)">x</button>
+        <button @click="removeActivity(activity)">x</button>
       </li>
     </ul>
   </div>
